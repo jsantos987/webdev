ca64b52a7e4aab6d724960317f4dee2fac16054a
jsantos@rackspace.com <jsantos@rackspace.com>
2018-07-05 02:04:59 -0500

    jQuery intro exercise

---
+++ training_projects/jQuery/jQueryExercise1.js
@@ -0,0 +1,12 @@
+// Select all divs and give them a purple background.
+$("div").css("background", "purple");
+
+// Select the divs with class "highlight" and make width 100 pixels.
+$("div.highlight").css("width", "100px");
+
+// Select div with id of third and give a 4 pixel solid oragne border.
+$("#third").css("border", "4px solid orange");
+
+
+// BONUS: Select the first div only and change font to pink.
+$("div:first-of-type").css("color", "pink");
