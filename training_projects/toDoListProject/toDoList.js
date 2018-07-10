431e125ef380534be987468c6e405adf98c2f11c
jsantos@rackspace.com <jsantos@rackspace.com>
2018-07-09 22:59:53 -0500

    ToDoList Update

--- training_projects/toDoListProject/toDoList.js
+++ training_projects/toDoListProject/toDoList.js
@@ -1,36 +1,22 @@
-/*
-// Check off specific todos by clicking
-$("li").click(function(){
-	// IF li is gray
-	if($(this).css("color") === "rgb(128, 128, 128)"){
-		// turn it black
-		$(this).css({
-			color: "black",
-			textDecoration: "none"
-		});
-	} 
-	// else
-	else {
-		$(this).css({
-			color: "gray",
-			textDecoration: "line-through"
-		});
-	}
-
-});
-*/
-
-// All the above refactored by creasting COMPLETED class and adding one line.
 // Check off Specific Todos By Clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
-
 // Click on X to delete ToDO
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		// grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		// create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>")
+	}
 });
