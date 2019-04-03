63d375caef873c709a1635ec26be2f5745506069
jsantos@rackspace.com <jsantos@rackspace.com>
2019-04-02 23:48:55 -0500

    Stopped setMessage

--- NUMBER_GUESS/app.js
+++ NUMBER_GUESS/app.js
@@ -1,1 +1,45 @@
-alert(1);
+/*
+GAME FUNCTION:
+- Player must guess a number between a min and max
+- Player gets a certain amount of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if loose
+- Let Player choose to play again
+*/
+
+// Game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Elements
+const UIgame = document.querySelector('#game'),
+      UIminNum = document.querySelector('.min-num'),
+      UImaxNum = document.querySelector('.max-num'),
+      UIguessBtn = document.querySelector('#guess-btn'),
+      UIguessInput = document.querySelector('#guess-input'),
+      UImessage = document.querySelector('.message');
+
+
+// Assign UI min and max
+UIminNum.textContent = min;
+UImaxNum.textContent = max;
+
+// Listen for guess
+UIguessBtn.addEventListener('click', function(){
+    let guess = parseInt(UIguessInput.value);
+    //console.log(guess);
+
+    // Validate
+    // If guess is not a number or guess is less than min or guess is greater than max
+    if(isNaN(guess) || guess < min || guess > max){
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+    }
+
+});
+// Set Message
+function setMessage(msg, color){
+    UImessage.style.color = color;
+    UImessage.textContent = msg;
+}
